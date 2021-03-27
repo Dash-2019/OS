7a44cd87a958e03bdab32c554c15bfc679272623
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-03-27 12:47:17 +0900

    Update first.sh

--- first.sh
+++ first.sh
@@ -2,13 +2,21 @@
 
 echo installing package 21/03/27
 << COMMENTOUT
+------------------------------------
 yes | sudo apt install curl
 yes | sudo apt install git
 
-COMMENTOUT
 
-# ----------------------------------
+
+#/////////////////////
 # Atom snap package is distributed and maintained by Github.
 # https://linuxize.com/post/how-to-install-atom-text-editor-on-ubuntu-20-04/
 echo installing Atom Snap Package
 yes | sudo snap install atom --classic
+
+------------------------------------
+COMMENTOUT
+
+echo install chromium
+sudo apt update
+yes | sudo apt install chromium-browser
