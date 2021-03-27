b308c6b37040777657ddfe5e6cfbd2d18e36a60b
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-03-27 14:39:25 +0900

    Update first.sh

--- first.sh
+++ first.sh
@@ -3,13 +3,19 @@
 echo installing package 21/03/27
 << COMMENTOUT
 #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+sudo apt update
 yes | sudo apt install curl
 yes | sudo apt install git
 yes | sudo snap install atom --classic
 
+
+sudo apt update
+yes | sudo apt install chromium-browser
+#yes | sudo snap install chromium
+
+
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 COMMENTOUT
 
-#sudo apt update
-#yes | sudo apt install chromium-browser
-yes | sudo snap install chromium
+sudo apt update
+yes | sudo apt install vlc
