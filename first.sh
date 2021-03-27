06ac31b7f738e31e5379fe747501fc05c29b3164
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-03-27 12:30:56 +0900

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo hello sou
+echo install curl
 << COMMENTOUT
 # sudo apt install curl
 yes | sudo apt install git
 
 COMMENTOUT
 
+# ----------------------------------
+# Atom snap package is distributed and maintained by Github.
+echo installing Atom Snap Package
 yes | sudo snap install atom --classic
