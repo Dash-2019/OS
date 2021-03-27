33260295a15b907fa667fa2eabed9c4866ee7af3
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-03-27 20:28:07 +0900

    Update first.sh

--- first.sh
+++ first.sh
@@ -37,6 +37,6 @@ COMMENTOUT
 wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo apt update
-sudo apt install -y atom
+yes | sudo apt install atom
 
 
