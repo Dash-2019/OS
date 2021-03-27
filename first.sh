dd95a98074725c13126f0eb67d4c68894f1d2836
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-03-27 15:14:57 +0900

    Update first.sh

--- first.sh
+++ first.sh
@@ -4,6 +4,7 @@ echo installing package 21/03/27
 << COMMENTOUT
 #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 sudo apt update
+
 yes | sudo apt install curl
 yes | sudo apt install git
 yes | sudo snap install atom --classic
@@ -13,9 +14,27 @@ sudo apt update
 yes | sudo apt install chromium-browser
 #yes | sudo snap install chromium
 
+sudo apt update
+sudo apt install -y vlc
+#sudo apt install -y vlc-plugin-access-extra libbluray-bdj libdvdcss2
+
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 COMMENTOUT
 
-sudo apt update
-sudo apt install -y vlc
+
+# or use　--------------------------
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
+wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+
+# add Typora's repository
+sudo add-apt-repository 'deb https://typora.io/linux ./'
+sudo apt-get update
+
+# install typora
+# sudo snap install typora
+sudo apt-get -y install typora
+
+
+
+
