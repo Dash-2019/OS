176dd1ee48a0e141f29360670534ed3306584420
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-04-03 07:17:23 +0900

    Create u_install.sh

---
+++ u_install.sh
@@ -0,0 +1,4 @@
+curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+sudo apt update
+sudo apt -y install syncthing
