52dbf0b5037ac27863c3b74b2e046517f4bec139
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-04-03 11:20:45 +0900

    Update u_install.sh

--- u_install.sh
+++ u_install.sh
@@ -1,4 +1,26 @@
+<< COMMENTOUT
 curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
 echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
 sudo apt update
 sudo apt -y install syncthing
+
+
+
+
+sudo apt install git cmake make xorg-dev g++ libcurl4-openssl-dev \
+                 libavahi-compat-libdnssd-dev libssl-dev libx11-dev \
+                 libqt4-dev qtbase5-dev
+
+git clone https://github.com/debauchee/barrier.git
+cd barrier
+./clean_build.sh
+cd build
+sudo make install
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+COMMENTOUT
+
+sudo add-apt-repository ppa:alexlarsson/flatpak
+sudo apt install flatpak gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
