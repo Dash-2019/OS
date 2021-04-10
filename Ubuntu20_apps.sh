86b7050f303b1add2f030ad78909fbb1c34637d5
Dash-2019 <soukou9@gmail.com>
2021-04-11 08:00:47 +0900

    teamviewr

--- Ubuntu20_apps.sh
+++ Ubuntu20_apps.sh
@@ -52,29 +52,15 @@ sudo apt install -y fish
 
 
 #10 -----------------------------------------
-#https://apt.syncthing.net/
-sudo curl -s -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
-
-# Add the "stable" channel to your APT sources:
-echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
-
-# Add the "candidate" channel to your APT sources:
-echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing candidate" | sudo tee /etc/apt/sources.list.d/syncthing.list
-
-# Increase preference of Syncthing's packages ("pinning")
-printf "Package: *\nPin: origin apt.syncthing.net\nPin-Priority: 990\n" | sudo tee /etc/apt/preferences.d/syncthing
-
-
-# Update and install syncthing:
-sudo apt-get update
-sudo apt-get install -y syncthing
-
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-COMMENTOUT
 
 curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
 echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
 sudo apt update
 sudo apt -y install syncthing
 
-#EE
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+COMMENTOUT
+
+#2021/4/11 (日) 7:47
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+sudo apt install ./teamviewer_amd64.deb -yes
