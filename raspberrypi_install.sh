ac17c7d4807b435fac9f3c5f5855e852b6610204
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-04-18 08:24:26 +0900

    Update raspberrypi_install.sh

--- raspberrypi_install.sh
+++ raspberrypi_install.sh
@@ -1,5 +1,18 @@
+#!/bin/bash
+
+<< COMMENTOUT
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<--
+
 # 2021/4/17 (土) 21:52
 sudo pip3 install selenium
 sudo apt-get install chromium-chromedriver
 sudo apt-get install fonts-ipafont-gothic fonts-ipafont-mincho
 
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+COMMENTOUT
+
+#-----------------------------------------
+#ステップ1 — install MySQL
+sudo apt install mysql-server -y
+#ステップ2 — MySQLの設定
+sudo mysql_secure_installation
