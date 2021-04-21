34422a23b98655ef5418888021a953155227ee18
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-04-21 20:12:44 +0900

    Create berryconda_setup.sh
    datareader

---
+++ berryconda_setup.sh
@@ -0,0 +1,10 @@
+
+
+# 2021/4/21-------
+#仮想環境
+conda create -n pienv
+source active pienv
+# install libary
+
+conda install numpy pandas pandas-datareader -y
+conda install lxml html5lib beautifulsoup4 -y
