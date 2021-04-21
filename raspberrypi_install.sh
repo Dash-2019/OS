9e7f0b95a8a6eef1270657a1408ebc06da243bb7
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-04-21 19:50:29 +0900

    Update raspberrypi_install.sh

--- raspberrypi_install.sh
+++ raspberrypi_install.sh
@@ -8,10 +8,16 @@ sudo pip3 install selenium
 sudo apt-get install chromium-chromedriver
 sudo apt-get install fonts-ipafont-gothic fonts-ipafont-mincho
 
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-COMMENTOUT
 
-#-----------------------------------------
+#2 mySQL  --------------------------------
 sudo apt-get install mariadb-server
 
+#3 berryconda   --------------------------
+wget https://github.com/jjhelmus/berryconda/releases/download/v2.0.0/Berryconda3-2.0.0-Linux-armv7l.sh
+chmod +x Berryconda3-2.0.0-Linux-armv7l.sh
+./Berryconda3-2.0.0-Linux-armv7l.sh
 
+
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+COMMENTOUT
