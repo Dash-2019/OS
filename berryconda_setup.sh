3a927f05e37883144d62dd0be370fdac38fdd02b
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-05-01 12:58:45 +0900

    Update berryconda_setup.sh
    gpio

--- berryconda_setup.sh
+++ berryconda_setup.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 << COMMENTOUT
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# 2021/4/21
+#仮想環境-------------------
 
-# 2021/4/21-------
-#仮想環境
-conda create -n pienv
-source active pienv
+#conda create -n pienv
+#source active pienv
 # install libary
 
 conda install numpy pandas pandas-datareader -y
 conda install lxml html5lib beautifulsoup4 -y
+pip install RPi.GPIO
 
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 COMMENTOUT
 
 
