06540c084e2986887d9446c14fcd0e8539d2ca64
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-05-01 21:15:06 +0900

    Update mac_setup.sh

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 echo mac_setup2021/3/30
+
+<< COMMENTOUT
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<--
+
 brew install fish
 brew install syncthing
 brew services start syncthing
 
 # 実行→ブラウザが立ち上がる
-syncthing
+# syncthing
+
+brew install --cask libreoffice
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+COMMENTOUT
