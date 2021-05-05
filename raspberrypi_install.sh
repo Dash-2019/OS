26f36f8076e71926882145c6c3176e076676abe4
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-05-05 22:59:52 +0900

    Update raspberrypi_install.sh
    docker

--- raspberrypi_install.sh
+++ raspberrypi_install.sh
@@ -17,25 +17,31 @@ wget https://github.com/jjhelmus/berryconda/releases/download/v2.0.0/Berryconda3
 chmod +x Berryconda3-2.0.0-Linux-armv7l.sh
 ./Berryconda3-2.0.0-Linux-armv7l.sh
 
-#4 Vim   --------------------------
+#4 Vim   --------------------------------
 
 sudo apt install -y vim
 
 
-#5 MySQL --------------------------
+#5 MySQL ------------------------------
 sudo apt-get install python-dev default-libmysqlclient-dev -y
 pip install mysqlclient
 
-#No module named sqlalchemy -----------------
+#No module named sqlalchemy ----------
 pip install flask_sqlalchemy
 
 #sudoなしで mysql -u root -p が動作するようにする
 # mariaDBにて設定する PDFあり
 
 
-#6 man 日本語変更｀ーーーーーーーーーーーーーーー
+#6 man 日本語変更｀ーーーーーーーーーーーーー
 sudo apt-get install manpages-ja
 
+
+
+
+
+# docker instal ----------------------------
+curl -sSL https://get.docker.com | sh
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 COMMENTOUT
