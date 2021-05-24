dae8973aa2ccfc431cfda19acddaf02b836e66d0
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-05-24 17:52:12 +0900

    Create conda_setup.ps1

---
+++ conda_setup.ps1
@@ -0,0 +1,16 @@
+echo powershell
+
+
+<< COMMENTOUT
+#echo 2021/4/24
+curl -sf https://raw.githubusercontent.com/ken-sayama/curl-sh/master/echo.sh  | sh -s
+hoge
+
+curl https://raw.githubusercontent.com/Dash-2019/pc_setup/master/conda_setup.ps1
+
+COMMENTOUT
+
+
+conda -V
+conda info 
+conda install numpy
