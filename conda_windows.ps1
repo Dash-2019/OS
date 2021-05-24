bad171cc1cf5fd6665035118e768d3acfce2440d
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-05-24 19:08:49 +0900

    Update and rename conda_setup.ps1 to conda_windows.ps1

--- conda_windows.ps1
+++ conda_windows.ps1
@@ -2,7 +2,7 @@ echo powershell
 #単一分のコメントです。
 #複数行 <#   #> 
 #エラー　改行コードはLFCR 
-#https://raw.githubusercontent.com/Dash-2019/pc_setup/master/conda_setup.ps1
+#https://raw.githubusercontent.com/Dash-2019/pc_setup/master/conda_windows.ps1
 
 conda -V
 #conda install numpy
