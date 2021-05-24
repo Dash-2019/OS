f2abfda12bcddf42f37b9eecfb19298720991cf3
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-05-24 18:05:49 +0900

    Update conda_setup.ps1

--- conda_setup.ps1
+++ conda_setup.ps1
@@ -1,16 +1,8 @@
 echo powershell
-
-
-<< COMMENTOUT
-#echo 2021/4/24
-curl -sf https://raw.githubusercontent.com/ken-sayama/curl-sh/master/echo.sh  | sh -s
-hoge
-
-curl https://raw.githubusercontent.com/Dash-2019/pc_setup/master/conda_setup.ps1
-
-COMMENTOUT
-
-
+#単一分のコメントです。
+<#
+複数行の コメントアウトです。
+https://raw.githubusercontent.com/Dash-2019/pc_setup/master/conda_setup.ps1
+#>
 conda -V
-conda info 
 conda install numpy
