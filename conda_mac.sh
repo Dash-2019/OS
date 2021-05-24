f63c7e773b6f56f65f5fe1d060cab39874522512
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-05-24 19:09:28 +0900

    Update and rename conda_setup_mac.sh to conda_mac.sh

--- conda_mac.sh
+++ conda_mac.sh
@@ -5,7 +5,7 @@
 curl -sf https://raw.githubusercontent.com/ken-sayama/curl-sh/master/echo.sh  | sh -s
 hoge
 
-curl https://raw.githubusercontent.com/Dash-2019/pc_setup/master/conda_setup_mac.sh | sh
+curl https://raw.githubusercontent.com/Dash-2019/pc_setup/master/conda_mac.sh | sh
 
 # 複数パッケージを同時にインストールできる。
 # pip install <package-name1> <package-name2> <package-name3> ...
