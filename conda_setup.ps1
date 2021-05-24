8f90d50b0d91a4fda596cb111e22f623e9a528e2
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-05-24 18:39:41 +0900

    Update conda_setup.ps1

--- conda_setup.ps1
+++ conda_setup.ps1
@@ -1,7 +1,8 @@
 echo powershell
 #単一分のコメントです。
-<#複数行の コメントアウトです。
-https://raw.githubusercontent.com/Dash-2019/pc_setup/master/conda_setup.ps1#>
+#複数行 <#   #> 
+#エラー　改行コードはLFCR 
+#https://raw.githubusercontent.com/Dash-2019/pc_setup/master/conda_setup.ps1
 
 conda -V
-conda install numpy
+#conda install numpy
