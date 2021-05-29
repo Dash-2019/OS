ccbc6079c42efe5ec7b3c62213d57d78b5f6e147
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-05-29 19:38:42 +0900

    Update Ubuntu20_Apps.sh

--- Ubuntu20_Apps.sh
+++ Ubuntu20_Apps.sh
@@ -123,6 +123,9 @@ wget https://download.xnview.com/XnViewMP-linux-x64.deb
 sudo apt install ./XnViewMP-linux-x64.deb
 
 sudo apt install geeqie
+
+#19 net-tools------------------------------
+sudo apt install net-tools
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 COMMENTOUT
 
