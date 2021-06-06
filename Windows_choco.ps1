25dd0901921b283c871091cc4cb49f35e54d2cdb
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-06-06 11:46:10 +0900

    Create Windows_choco.ps1

---
+++ Windows_choco.ps1
@@ -0,0 +1,6 @@
+# インストールchoco
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install powershell-core --yes
+choco install vscode --yes
+
