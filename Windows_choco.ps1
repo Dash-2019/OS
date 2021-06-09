250d253356e4582d88cb37c45abd6163fc8e1be7
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-06-09 19:55:08 +0900

    Update Windows_choco.ps1

--- Windows_choco.ps1
+++ Windows_choco.ps1
@@ -3,6 +3,14 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 
 choco install atom vim vscode powershell-core --yes
 
-choco instal vlc python git googlechrome --yes
+choco instal vlc python  --yes
 
-choco install winscp.install cygwin --yes
+choco install winscp.install rufus hwinfo.install greenshot --yes
+
+choco install autoruns irfanview everything --yes
+
+choco install docker-desktop github git.install --yes
+
+choco install vnc-viewer anydesk.install --yes
+
+choco install googlechrome firefox --yes
