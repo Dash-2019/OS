971b3178288ef7b366929375c0f55b64dd3ffa53
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-06-09 18:37:34 +0900

    Update Windows_choco.ps1

--- Windows_choco.ps1
+++ Windows_choco.ps1
@@ -1,6 +1,8 @@
 # インストールchoco
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install powershell-core --yes
-choco install vscode --yes
+choco install atom vim vscode powershell-core --yes
 
+choco instal vlc python git googlechrome --yes
+
+choco install winscp.install cygwin --yes
