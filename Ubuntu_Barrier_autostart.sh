ce325abcf74cba74ee32f18604052862d22e5702
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-06-19 12:23:55 +0900

    2

--- Ubuntu_Barrier_autostart.sh
+++ Ubuntu_Barrier_autostart.sh
@@ -6,5 +6,6 @@
 # sudo snap install barrier
 # https://github.com/debauchee/barrier/issues/578
 
+# 改行コードLF
 # 自動的に起動するプログラムの追加に
 # bash  filename.sh
--- Windows_basic_2106.ps1
+++ Windows_basic_2106.ps1
@@ -1,25 +1,21 @@
-# インストールchoco
 
 #choco upgrade chocolatey
+echo hello PowerShell
 
+<#
 choco install atom vim vscode typora --yes
 
 choco install powershell-core rpi-imager--yes
 
 choco instal vlc greenshot --yes
 
-# choco install python anaconda3 rlogin --yes
-
 choco install winscp.install rufus hwinfo.install greenshot --yes
 
 choco install autoruns irfanview everything --yes
 
-choco install docker-desktop github git.install virtualbox --yes
+choco install googlechrome firefox --yes
 
-choco install mysql --yes
-
-choco install vnc-viewer anydesk.install --yes
-
-# choco install googlechrome firefox --yes
-
+# OpenSSH server
 choco install openssh -params '"/SSHServerFeature"' -confirm
+
+#>
