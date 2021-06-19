7dac6d1c5aba0d6595ddcbb0421fa32e6ea0e8a4
Haru <46810488+Dash-2019@users.noreply.github.com>
2021-06-19 14:00:21 +0900

    ;

--- Ubuntu_Basic.sh
+++ Ubuntu_Basic.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-
 << COMMENTOUT
-
-#curl https://raw.githubusercontent.com/Dash-2019/pc_setup/master/Ubuntu_Env_Setup.sh |sh -s
-
+#curl url |sh -s
+#url= https://raw.githubusercontent.com/Dash-2019/pc_setup/master/Ubuntu_Basic.sh
 #ロックスクリーンを無効に。以下のコマンドを実行します。
 gsettings set org.gnome.desktop.lockdown disable-lock-screen true
 #サスペンド時の画面ロックを無効。
 gsettings set org.gnome.desktop.screensaver ubuntu-lock-on-suspend false
 #ブランクスクリーンからの復帰時の設定。
 gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
-#type $env:USERPROFILE\.ssh\id_rsa.pub |
-# ssh momo@192.168.10.121 "cat >> .ssh/authorized_keys"
 
 COMMENTOUT
 
