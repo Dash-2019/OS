fbd255c60d3dba90e6b448923f48fb0defa7a522
Haru <46810488+Dash-2019@users.noreply.github.com>
2022-02-02 12:51:22 +0900

    Update and rename Pi_setup.sh to Raspberry_Pi_setup.sh

--- Raspberry_Pi_setup.sh
+++ Raspberry_Pi_setup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 << COMMENTOUT
-curl https://raw.githubusercontent.com/Dash-2019/pc_setup/master/RaspberryPi_Apps.sh | sh -s
+curl -sf https://raw.githubusercontent.com/Dash-2019/pc_setup/master/Raspberry_Pi_setup.sh | sh -s
 
 # 2021/4/17 (土) 21:52
 sudo pip3 install selenium
 sudo apt-get install chromium-chromedriver
 sudo apt-get install fonts-ipafont-gothic fonts-ipafont-mincho
-
 
 #2 mySQL  --------------------------------
 sudo apt-get install mariadb-server
@@ -18,9 +17,7 @@ chmod +x Berryconda3-2.0.0-Linux-armv7l.sh
 ./Berryconda3-2.0.0-Linux-armv7l.sh
 
 #4 Vim   --------------------------------
-
 sudo apt install -y vim
-
 
 #5 MySQL ------------------------------
 sudo apt-get install python-dev default-libmysqlclient-dev -y
@@ -32,24 +29,12 @@ pip install flask_sqlalchemy
 #sudoなしで mysql -u root -p が動作するようにする
 # mariaDBにて設定する PDFあり
 
-
-#6 man 日本語変更｀ーーーーーーーーーーーーー
-sudo apt-get install manpages-ja
-
-
-
-
-
 # docker instal ----------------------------
 curl -sSL https://get.docker.com | sh
 
-
-
 # samba----------------
-
 sudo apt-get update
 sudo apt-get install samba samba-common-bin
-
 
 # web server apache2---------------
 sudo apt update
@@ -71,9 +56,10 @@ sudo mv wordpress/* .
 sudo rm latest.tar.gz
 sudo rmdir wordpress
 
-
-
-
-
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 COMMENTOUT
+
+sudo apt update
+sudo apt install -y vim
+
+
