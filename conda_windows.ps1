16ea7f9ac02650a3c13c36bce9517eef7cf5ec40
Haru <46810488+Dash-2019@users.noreply.github.com>
2022-12-28 04:25:24 +0900

    Update conda_windows.ps1

--- conda_windows.ps1
+++ conda_windows.ps1
@@ -1,8 +1,4 @@
+# Git pc_setup
 echo powershell
-#単一分のコメントです。
-#複数行 <#   #> 
-#エラー　改行コードはLFCR 
-#https://raw.githubusercontent.com/Dash-2019/pc_setup/master/conda_windows.ps1
-
-conda -V
-#conda install numpy
+echo test
+date
