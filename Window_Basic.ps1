a1b0b30e0f93cfa770069153627cab655dda1fc7
Haru <46810488+Dash-2019@users.noreply.github.com>
2022-12-28 06:18:46 +0900

    Update Window_Basic.ps1

--- Window_Basic.ps1
+++ Window_Basic.ps1
@@ -1,19 +1,12 @@
 <# ----------------
- 
-# Editor----------------------------------
-choco install atom vscode sublimetext4 typora --yes
-choco install -y hidemaru-editor  mery notepadplusplus --yes
- 
-# Utility--------------------------------
-choco install mousewithoutborders strokesplus everything --yes
- 
-choco upgrade notepadplusplusplus --yes
- 
------------------#>
-
-#Brower-----------------------------------
+#Brower----------------
 choco install googlechrome firefox --yes
  
-# Multiplayer-----------------------------
-choco instal vlc --yes
-choco install hidemaru-editor  mery notepadplusplus --yes
+choco install notepadplusplus --yes
+choco install f.lux  
+ -----------------#>
+
+# Utility-------------
+choco install mousewithoutborders strokesplus everything --yes
+# Editor--------------
+choco install atom vscode typora　sublimetext4 --y
