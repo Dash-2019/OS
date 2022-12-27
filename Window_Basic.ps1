0586240af822d9d8d8fd78d0d36c1d20814cc4f6
Haru <46810488+Dash-2019@users.noreply.github.com>
2022-12-28 06:42:15 +0900

    Update Window_Basic.ps1

--- Window_Basic.ps1
+++ Window_Basic.ps1
@@ -1,12 +1,18 @@
-<# ----------------
+<#  
+===============================
 #Brower----------------
 choco install googlechrome firefox --yes
- 
-choco install notepadplusplus --yes
-choco install f.lux  
- -----------------#>
 
 # Utility-------------
 choco install mousewithoutborders strokesplus everything --yes
+choco install f.lux hwinfo autohotkey autoit --y
+
 # Editor--------------
-choco install atom vscode typora　sublimetext4 --y
+choco install atom vscode sublimetext4 --yes
+choco install notepadplusplus --yes
+
+===============================
+#>
+
+choco install beeftext qdir displayfusion --y
+
