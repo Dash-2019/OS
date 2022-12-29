b528771aa582110c9fcb2870375e8eb231e1621a
Haru <46810488+Dash-2019@users.noreply.github.com>
2022-12-29 20:51:24 +0900

    Update Window_Basic.ps1

--- Window_Basic.ps1
+++ Window_Basic.ps1
@@ -6,6 +6,7 @@ choco install googlechrome firefox --yes
 # Utility-------------
 choco install mousewithoutborders strokesplus everything --yes
 choco install f.lux hwinfo autohotkey autoit --y
+choco install beeftext qdir displayfusion --y
 
 # Editor--------------
 choco install atom vscode sublimetext4 --yes
@@ -14,5 +15,5 @@ choco install notepadplusplus --yes
 ===============================
 #>
 
-choco install beeftext qdir displayfusion --y
+choco install obsidian bitcomet --y
 
