0d456575a0aa1d26834c875449e89a9317e02897
Haru <46810488+Dash-2019@users.noreply.github.com>
2022-12-30 17:34:59 +0900

    Update Window_Basic.ps1

--- Window_Basic.ps1
+++ Window_Basic.ps1
@@ -1,10 +1,14 @@
 <#  
 ===============================
+choco install mousewithoutborders strokesplus everything --yes
+===============================
+#>
+
 #Brower----------------
 choco install googlechrome firefox --yes
 
 # Utility-------------
-choco install mousewithoutborders strokesplus everything --yes
+choco install mousewithoutborders strokesplus --yes
 choco install f.lux hwinfo autohotkey autoit --y
 choco install beeftext qdir displayfusion --y
 
@@ -14,7 +18,5 @@ choco install atom vscode sublimetext4 notepadplusplus --yes
 choco install obsidian bitcomet --y
 choco install google-pinyin rlogin winscp --y
 
-===============================
-#>
 
 choco install simple-sticky-notes --y
