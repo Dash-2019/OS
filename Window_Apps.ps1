8d77145bac0abd5f0d5969236c54d2c505d14b5b
Haru <46810488+Dash-2019@users.noreply.github.com>
2023-01-01 21:39:25 +0900

    Update Window_Apps.ps1

--- Window_Apps.ps1
+++ Window_Apps.ps1
@@ -7,21 +7,25 @@ choco install googlechrome firefox --yes
 
 # Utility-------------
 choco install mousewithoutborders strokesplus --y
-
 choco install ultrasearch everything --yes
-
 choco install f.lux hwinfo autohotkey autoit --y
 choco install beeftext qdir displayfusion --y
+choco install winrar bitcomet keepassxc --y
 
 # Editor--------------
 choco install atom vscode sublimetext4 notepadplusplus --yes
 
-choco install obsidian bitcomet keepassxc --y
-#choco install rlogin winscp 
-choco install winrar libreoffice-fresh simple-sticky-notes --y
+# Office
+choco install libreoffice-fresh simple-sticky-notes --y
+choco install obsidian --y
+choco install greenshot imageglass --y
+
+# Server -----------------
+#choco install rlogin winscp --y
+
+# Programming
+choco install adobereader dropbox --y
+choco install python anaconda3 mariadb --y
 
 ===============================
 #>
-
-choco install adobereader dropbox --y
-choco install python anaconda3 --y
