13ecb243cdc640bf497abdbff5362b7f4dc536a8
Haru <46810488+Dash-2019@users.noreply.github.com>
2023-02-14 08:16:55 +0900

    Create Windows_Basic_Setup.ps1
    2023/02/14 basic

---
+++ Windows_Basic_Setup.ps1
@@ -0,0 +1,21 @@
+<# 
+#Run as Admin
+#>
+
+choco install googlechrome firefox vlc --yes
+
+# Utility-------------
+choco install mousewithoutborders strokesplus --y
+choco install ultrasearch everything --yes
+choco install f.lux hwinfo autohotkey autoit --y
+choco install beeftext qdir --y
+choco install winrar bitcomet keepassxc --y
+choco install simple-sticky-notes --y
+
+# Editor--------------
+choco install obsidian vscode sublimetext4 notepadplusplus --yes
+
+
+# Photo--------------
+choco install imageglass --y
+choco install picpick.portable --y
