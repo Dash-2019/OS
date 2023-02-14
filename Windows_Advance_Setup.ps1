c3ba84a0ef3bee85a2f8d44ccfcad9da50608f41
Haru <46810488+Dash-2019@users.noreply.github.com>
2023-02-14 09:06:19 +0900

    Update and rename Window_Apps.ps1 to Windows_Advance_Setup.ps1

---
+++ Windows_Advance_Setup.ps1
@@ -0,0 +1,17 @@
+<# 
+#Run as Admin
+2023/02/14
+#>
+
+# Office
+choco install libreoffice-fresh 
+choco install dropbox --y
+
+
+# Programming
+choco install python anaconda3 mariadb --y
+choco install r r.studio --y
+
+#eBooks
+choco install calibre -y
+
