b14db9fd427f46c29933d3f6bc6b798375f848c3
Haru <46810488+Dash-2019@users.noreply.github.com>
2023-02-14 09:16:55 +0900

    Update and rename Windows_Basic_Setup.ps1 to Windows_Setup.ps1

--- Windows_Setup.ps1
+++ Windows_Setup.ps1
@@ -2,13 +2,13 @@
 -------------------
 2023/02/14
 -------------------
-#>
+
 #Run as Admin
 
 choco install firefox vlc --yes
 
 # Utility-------------
-choco install mousewithoutborders strokesplus --y
+choco install strokesplus --y
 choco install ultrasearch everything --yes
 choco install f.lux autohotkey autoit --y
 choco install beeftext qdir --y
@@ -18,6 +18,26 @@ choco install simple-sticky-notes --y
 # Editor--------------
 choco install obsidian vscode sublimetext4 notepadplusplus --yes
 
+#eBooks
+choco install calibre -y
+choco install mousewithoutborders --y
 
 # Photo--------------
 choco install imageglass --y
+
+
+
+# ===========================
+# Advance Setup
+# ===========================
+
+#>
+
+# Office
+choco install libreoffice-fresh 
+choco install dropbox --y
+
+# Programming
+choco install python anaconda3 mariadb --y
+choco install r r.studio --y
+
