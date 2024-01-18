6b51ee99504d72897ddcdfbcf0711302e0b9cb81
Haru <46810488+Dash-2019@users.noreply.github.com>
2024-01-18 09:29:45 +0900

    Update Windows_Setup.ps1

--- Windows_Setup.ps1
+++ Windows_Setup.ps1
@@ -2,15 +2,16 @@
 -------------------
 2023/02/14
 -------------------
-
+#>
 #Run as Admin
+#choco install avastfreeantivirus -y
 
 choco install firefox vlc --yes
 
 # Utility-------------
-choco install strokesplus --y
-choco install ultrasearch everything --yes
-choco install f.lux autohotkey autoit --y
+choco install strokesplus everything -y
+#choco install ultrasearch --yes
+#choco install f.lux autohotkey autoit --y
 choco install beeftext qdir --y
 choco install winrar bitcomet keepassxc --y
 choco install simple-sticky-notes --y
@@ -19,25 +20,20 @@ choco install simple-sticky-notes --y
 choco install obsidian vscode sublimetext4 notepadplusplus --yes
 
 #eBooks
-choco install calibre -y
-choco install mousewithoutborders --y
+#choco install calibre -y
+# choco install mousewithoutborders --y
 
 # Photo--------------
 choco install imageglass --y
-
-
-
-# ===========================
-# Advance Setup
-# ===========================
+choco install xnviewmp irfanview fsviewer -y
 
 #>
-
 # Office
 choco install libreoffice-fresh 
-choco install dropbox --y
+# choco install dropbox --y
 
 # Programming
-choco install python anaconda3 mariadb --y
+ choco install python anaconda3 mariadb --y
 choco install r r.studio --y
 
+#>
