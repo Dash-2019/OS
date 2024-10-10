29e58aa0e3081b29960c338e86faa5d2e0567f48
Haru <46810488+Dash-2019@users.noreply.github.com>
2024-10-10 21:57:49 +0900

    Create scoop.ps1
    11

---
+++ scoop.ps1
@@ -0,0 +1,13 @@
+# install scoop
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+
+# install basic module
+scoop install aria2
+
+# add bucket
+scoop bucket add extras
+scoop bucket add versions
+
+scoop install extras/potplayer
+scoop install 7zip sudo git time openssl
