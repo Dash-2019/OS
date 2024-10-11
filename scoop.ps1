054e4d7ee4bda6dbbc033c2b920f87c599060ffb
Haru <46810488+Dash-2019@users.noreply.github.com>
2024-10-11 21:34:43 +0900

    Update scoop.ps1
    99

--- scoop.ps1
+++ scoop.ps1
@@ -1,14 +1,11 @@
-# install choco -----------------------------------------------
+# ----------------------------------------------------
+# install choco
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-# install Scoop -----------------------------------------------
+# ----------------------------------------------------
+# install Scoop
 # インストールディレクトリの設定 (user)
 #$env:SCOOP='C:\abc'
 #[Environment]::SetEnvironmentVariable('SCOOP', $env:SCOOP, 'User')
-
-# インストールディレクトリの設定 (global)
-#$env:SCOOP_GLOBAL='D:\GlobalScoopApps'
-#[Environment]::SetEnvironmentVariable('SCOOP_GLOBAL', $env:SCOOP_GLOBAL, 'Machine')
 try {
   # Scoopのインストール確認
   get-command scoop -ErrorAction Stop
@@ -18,20 +15,20 @@ catch [Exception] {
   Set-ExecutionPolicy RemoteSigned -scope CurrentUser
   Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 }
-
-# install basic module ------------------------------
+# ----------------------------------------------------
+# install basic module
 scoop install aria2
 scoop install git
 scoop install sudo
 scoop install 7zip
-
-# add bucket----------------------------------------
+# ----------------------------------------------------
+# add bucket
 scoop bucket add extras
 scoop bucket add versions
 scoop bucket add sysinternals
 scoop bucket add dorado https://github.com/chawyehsu/dorado
-
-# add Apps--------------------------------------------
+# ----------------------------------------------------
+# add Apps
 scoop install autologon
 scoop install beeftext
 scoop install brave
