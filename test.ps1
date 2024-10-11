3012c4c39ea373dc39a3ed4c13de4c111cbedbf1
Haru <46810488+Dash-2019@users.noreply.github.com>
2024-10-11 21:18:58 +0900

    Update and rename test.sh to test.ps1
    25

---
+++ test.ps1
@@ -0,0 +1,13 @@
+
+try {
+  # Scoopのインストール確認
+  get-command scoop -ErrorAction Stop
+} 
+catch [Exception] {
+  # Scoopのインストール
+  Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+  Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+}
+
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
--- test.sh
+++
@@ -1,2 +0,0 @@
-scoop list
-scoop help
