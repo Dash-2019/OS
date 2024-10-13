83ccb392277e324758a682c8d8736abbf27218be
Haru <46810488+Dash-2019@users.noreply.github.com>
2024-10-13 22:21:58 +0900

    Update Ubuntu_Basic.sh

--- Ubuntu_Basic.sh
+++ Ubuntu_Basic.sh
@@ -17,10 +17,21 @@ gsettings set org.gnome.desktop.screensaver ubuntu-lock-on-suspend false
 #ブランクスクリーンからの復帰時の設定。
 gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
 
-COMMENTOUT
 
 # Brave
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install brave-browser -y
+
+COMMENTOUT
+
+# sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code -y w# or code-insiders
+
