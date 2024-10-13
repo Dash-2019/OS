a034f9ea11fe22d6e3cff5813ea815b9e1151ab6
Haru <46810488+Dash-2019@users.noreply.github.com>
2024-10-13 20:06:08 +0900

    Update Ubuntu_Basic.sh

--- Ubuntu_Basic.sh
+++ Ubuntu_Basic.sh
@@ -5,8 +5,9 @@
 echo 1907 | sudo -S apt install openssh-server -y
 sudo systemctl start ssh
 sudo systemctl enable ssh
+# 1 barrier sharemouse
+sudo apt install barrier
 
-COMMENTOUT
 #curl url |sh -s
 #url= https://raw.githubusercontent.com/Dash-2019/pc_setup/master/Ubuntu_Basic.sh
 #ロックスクリーンを無効に。以下のコマンドを実行します。
@@ -16,6 +17,10 @@ gsettings set org.gnome.desktop.screensaver ubuntu-lock-on-suspend false
 #ブランクスクリーンからの復帰時の設定。
 gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
 
-# 1 barrier sharemouse
-sudo apt install barrier
+COMMENTOUT
 
+# Brave
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
