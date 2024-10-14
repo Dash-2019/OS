85a80b0c6f2e52091ab167ce5f45a63af16bdb6b
Haru <46810488+Dash-2019@users.noreply.github.com>
2024-10-14 10:31:45 +0900

    Update WinApps.ps1

--- WinApps.ps1
+++ WinApps.ps1
@@ -1,51 +1,31 @@
-# 管理者権限で実行
-# sousoukou----------------------------------------------------
-# install Scoop
+# install Scoop choco管理者権限で実行-----------------------
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
-# ----------------------------------------------------
-# install basic module
-scoop install aria2
-scoop install git
-scoop install sudo
-scoop install 7zip
-# ----------------------------------------------------
-# add bucket
+# add bucket-------------------------------------------
 scoop bucket add extras
 scoop bucket add versions
 scoop bucket add sysinternals
 scoop bucket add dorado https://github.com/chawyehsu/dorado
 scoop bucket add iyokan-jp https://github.com/tetradice/scoop-iyokan-jp
 scoop bucket add nonportable
-# ----------------------------------------------------
-# add Apps
-scoop install autologon
-#scoop install extras/barrier
-scoop install beeftext
-scoop install brave
-#scoop install nonportable/dropbox-np
+# basic module -------------------------------------------
+scoop install 7zip aria2 git sudo #basic module
+scoop install anydesk autologon f.lux
+#scoop install barrier
+scoop install beeftext espanso everything
+scoop install brave googlechrome
 scoop install eartrumpet
-scoop install espanso
-scoop install everything
-scoop install f.lux
 scoop install foxit-pdf-reader
-scoop install Groonga #全文検索
-scoop install googlechrome
-scoop install greenshot
-scoop install irfanview
-scoop install inazumasearch　#全文検索
+scoop install greenshot irfanview
+scoop install Groonga inazumasearch #全文検索
 scoop install keepassxc
 scoop install libreoffice
-scoop install mery
-scoop install notepadplusplus
-scoop install potplayer
-scoop install powershell
-scoop install vlc
-scoop install vscode
+scoop install mery notepadplusplus
+scoop install potplayer vlc
+scoop install vscode sublime-text
 scoop install xnviewmp
-#>
-# ----------------------------------------------------
-# install choco
+
+# install choco ------------------------------------
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco install bitcomet -y
@@ -54,8 +34,6 @@ choco install displayfusion -y
 choco install mousewithoutborders -y
 choco install googledrive -y
 choco install qdir -y
+choco install powershell-core
+
 #choco install avastfreeantivirus
-
-
-
-
