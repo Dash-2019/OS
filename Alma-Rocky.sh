e95d05f5c7bb6d464ac6338b4386eb42b29314c4
Haru <46810488+Dash-2019@users.noreply.github.com>
2024-10-19 22:44:27 +0900

    Update Alma-Rocky.sh

--- Alma-Rocky.sh
+++ Alma-Rocky.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+sudo dnf -y install epel-release
+sudo dnf upgrade
+sudo dnf -y install vlc
+
+sudo dnf -y install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
