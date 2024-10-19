53190d397806a9292ae6d783062c773d2f8388d8
Haru <46810488+Dash-2019@users.noreply.github.com>
2024-10-19 23:25:16 +0900

    Update Alma-Rocky.sh

--- Alma-Rocky.sh
+++ Alma-Rocky.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 << COMMENTOUT
-
-sudo dnf -y install epel-release
-sudo dnf update -y
-sudo dnf -y install vlc
-
-COMMENTOUT
-#sudo dnf -y install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+sudo dnf -y install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 
 sudo dnf update -y
 sudo dnf install dnf-plugins-core #Step 2: Install required package
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf install brave-browser
+COMMENTOUT
+
+sudo dnf install brave-browser -y
+
+# VLC Player
+sudo dnf -y install epel-release
+sudo dnf update -y
+sudo dnf -y install vlc
+
