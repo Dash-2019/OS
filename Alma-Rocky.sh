be1e92fe1d300d1f56ffd6adc2ffa35870c3d5f2
Haru <46810488+Dash-2019@users.noreply.github.com>
2024-10-19 23:26:16 +0900

    Update Alma-Rocky.sh

--- Alma-Rocky.sh
+++ Alma-Rocky.sh
@@ -7,9 +7,8 @@ sudo dnf update -y
 sudo dnf install dnf-plugins-core #Step 2: Install required package
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo dnf -y install brave-browser
 COMMENTOUT
-
-sudo dnf install brave-browser -y
 
 # VLC Player
 sudo dnf -y install epel-release
