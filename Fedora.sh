a34129315c7801f5a5eef2c2c4406db01fba85df
Haru <46810488+Dash-2019@users.noreply.github.com>
2024-10-21 12:49:30 +0900

    Update Fedora.sh

--- Fedora.sh
+++ Fedora.sh
@@ -5,3 +5,5 @@ COMMENTOUT
 sudo dnf copr enable eclipseo/espanso
 sudo dnf install espanso espanso-wayland
 sudo setcap "cap_dac_override+p" $(which espanso-wayland)
+
+sudo snap install nap-snippets
