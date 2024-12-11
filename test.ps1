afabf8ffc89bdcf0e0a1a1a9c9f6db40d1b192d2
Haru <46810488+Dash-2019@users.noreply.github.com>
2024-12-11 21:36:27 +0900

    Create test.ps1
    d

---
+++ test.ps1
@@ -0,0 +1,9 @@
+# add bucket-------------------------------------------
+scoop bucket add extras
+scoop bucket add versions
+scoop bucket add sysinternals
+scoop bucket add dorado https://github.com/chawyehsu/dorado
+scoop bucket add iyokan-jp https://github.com/tetradice/scoop-iyokan-jp
+scoop bucket add nonportable
+# basic module -------------------------------------------
+scoop install 7zip aria2 git sudo #basic module
