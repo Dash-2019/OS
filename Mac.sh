d21e59a4f1aa1fcea9e21525706c461420f6d7ba
Haru <46810488+Dash-2019@users.noreply.github.com>
2025-01-13 17:40:24 +0900

    Update Mac.sh

--- Mac.sh
+++ Mac.sh
@@ -3,9 +3,9 @@
 複数行コメントアウト
 echo "Hi, Jiro!"
 curl -sf https://raw.githubusercontent.com/Dash-2019/OS/refs/heads/master/Mac.sh | sh -s
-brew install --cask google-drive
 COMMENTOUT
 
 echo "Hi, install start--!" 
-# Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Homebrew git無理
+brew install --cask google-chrome
+brew install --cask google-drive
