c40ec043c8201be6b7aa83e3ef0e163575d9513f
Haru <46810488+Dash-2019@users.noreply.github.com>
2025-01-13 16:59:09 +0900

    Update Mac.sh

--- Mac.sh
+++ Mac.sh
@@ -4,7 +4,7 @@
 << COMMENTOUT
 複数行コメントアウト
 echo "Hi, Jiro!"
-curl -sf https://raw.githubusercontent.com/Dash-2019/OS/refs/heads/master/Alma-Rocky.sh | sh -s
+curl -sf https://raw.githubusercontent.com/Dash-2019/OS/refs/heads/master/Mac.sh | sh -s
 brew install --cask google-drive
 COMMENTOUT
 
