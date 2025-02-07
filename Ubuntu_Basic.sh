1f391a7b6fae7e0b5a90f215ded9adafafd4579a
Haru <46810488+Dash-2019@users.noreply.github.com>
2025-02-07 22:45:57 +0900

    Update Ubuntu_Basic.sh

--- Ubuntu_Basic.sh
+++ Ubuntu_Basic.sh
@@ -19,10 +19,7 @@ gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
 
 
 # Brave
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser -y
+
 
 
 # sudo apt-get install wget gpg
