3c14784d6e5d85efb4f466527f853858490a6e8d
Haru <46810488+Dash-2019@users.noreply.github.com>
2025-09-08 12:50:08 +0900

    Update WSL-Ubuntu.ps1

--- WSL-Ubuntu.ps1
+++ WSL-Ubuntu.ps1
@@ -2,7 +2,7 @@
 
 echo installing package 2025/09/08
 << COMMENTOUT
-curl -sf https://raw.githubusercontent.com/Dash-2019/OS/refs/heads/master/WSL-Ubuntu.ps1h | sh -s
+curl -sf https://raw.githubusercontent.com/Dash-2019/OS/refs/heads/master/WSL-Ubuntu.sh | sh -s
 
 COMMENTOUT
 echo --end--WSL-Ubuntu-
