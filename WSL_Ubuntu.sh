2c29ceaa75010d759c266028b4a5fc19988ab074
Haru <46810488+Dash-2019@users.noreply.github.com>
2025-09-08 13:00:15 +0900

    Update WSL_Ubuntu.sh

--- WSL_Ubuntu.sh
+++ WSL_Ubuntu.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
-
-echo installing package 2025/09/08
+echo installing start -------
 << COMMENTOUT
+2025/09/08
 curl -sf https://raw.githubusercontent.com/Dash-2019/OS/refs/heads/master/WSL_Ubuntu.sh | sh -s
-
 COMMENTOUT
-echo --end--WSL-Ubuntu-
+
+# 01 WSL2のUbuntuにDockerをインストールする
+#--- Dockerのaptリポジトリからインストールする
+sudo apt-get update
+sudo apt-get upgrade -y
+#--- Dockerをインストールするために必要なパッケージをインストールしていきます。
+sudo apt-get install ca-certificates gnupg
+#--- Docker公式のGPGキーを登録していきます。
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+#--- Docker公式のGPGキーを使って、aptが参照するリポジトリを追加します。
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#--- 追加したリポジトリの情報を取得するために、再度aptのアップデートを行いdockerをインストールします
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-pluginsudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+#=========================
+echo ===END===WSL_Ubuntu
