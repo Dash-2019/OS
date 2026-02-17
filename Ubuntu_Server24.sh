19f02d0fe567777f735a0cc6c76333e46f690168
Haru <46810488+Dash-2019@users.noreply.github.com>
2026-02-17 19:51:59 +0900

    Update Ubuntu_Server24.sh
    re3

--- Ubuntu_Server24.sh
+++ Ubuntu_Server24.sh
@@ -3,16 +3,32 @@
 curl -sf https://raw.githubusercontent.com/Dash-2019/OS/refs/heads/master/Ubuntu_Server24.sh | sh -s
 COMMENTOUT
 
-# 1 docker
-# 必須ソフトウェアインストール
-sudo apt update && sudo apt install -y ca-certificates curl gnupg lsb-release
-#GPGキー取得
-sudo mkdir -m 0755 -p /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-#リポジトリの設定
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#Docker Engineインストール
-sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-#動作確認
-#sudo docker run hello-world
+# Dockerのリポジトリを設定する
+#1 apt パッケージを更新します。
+sudo apt update
 
+#2 必要なパッケージをインストールします
+sudo apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+# 3.Docker 公式の GPG 公開鍵をインストールします。
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# 4 fingerprint 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88であることを確認します。
+sudo apt-key fingerprint 0EBFCD88
+
+# 5. repository (stable) を追加します。(x86_64 / amd64 アーキテクチャの場合
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+
+# Docker をインストールする
+# 最新版のDocker をインストールします。
+sudo apt update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
