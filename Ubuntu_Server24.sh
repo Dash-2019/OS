b66fc1557ff8116c7298885794f0e713ded902b8
Haru <46810488+Dash-2019@users.noreply.github.com>
2026-02-17 19:55:10 +0900

    Update Ubuntu_Server24.sh
    r4

--- Ubuntu_Server24.sh
+++ Ubuntu_Server24.sh
@@ -5,15 +5,10 @@ COMMENTOUT
 
 # Dockerのリポジトリを設定する
 #1 apt パッケージを更新します。
-sudo apt update
+#sudo apt update
 
 #2 必要なパッケージをインストールします
-sudo apt install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+sudo apt install apt-transport-https ca-certificates gnupg-agent software-properties-common
 
 # 3.Docker 公式の GPG 公開鍵をインストールします。
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -21,13 +16,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 # 4 fingerprint 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88であることを確認します。
 sudo apt-key fingerprint 0EBFCD88
 
-# 5. repository (stable) を追加します。(x86_64 / amd64 アーキテクチャの場合
-sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
+# 5. repository (stable) を追加します
+sudo add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-# Docker をインストールする
 # 最新版のDocker をインストールします。
 sudo apt update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
