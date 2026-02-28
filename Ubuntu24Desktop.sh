28df3b05cfe67fe71b452990d67f7661f6d489d0
Haru <46810488+Dash-2019@users.noreply.github.com>
2026-03-01 07:38:32 +0900

    Update and rename Ubuntu_Basic.sh to Ubuntu24Desktop.sh
    docker

---
+++ Ubuntu24Desktop.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+<< COMMENTOUT
+シェルスプリプトや自動化、aptよりもapt-getを使用することが推奨されています。 
+curl -sf https://raw.githubusercontent.com/Dash-2019/pc_setup/master/Ubuntu20_Apps.sh | sh -s
+
+# curl install------
+sudo apt update && sudo apt-get install -y curl
+
+# ssh server ==============
+sudo apt-get install -y openssh-server
+sudo systemctl start ssh
+sudo systemctl enable ssh
+
+COMMENTOUT
+#docker ===================
+#①Dockerをインストールするために、まず依存関係のあるパッケージをインストールします。
+sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg lsb-release
+
+# ②次に、以下のコマンドによりDockerの公式GPGキーを追加します。
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+#③Dockerのリポジトリを登録します。以下のコマンドを実行しましょう。
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+#④DockerとDocker Composeをインストールします。
+sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+
+
