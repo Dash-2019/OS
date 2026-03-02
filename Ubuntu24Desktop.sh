fe02a7eb1cf6f6f03517cac31abc3f5cad88c1f0
Haru <46810488+Dash-2019@users.noreply.github.com>
2026-03-02 21:27:13 +0900

    Add installation steps for Caddy server
    fsf

--- Ubuntu24Desktop.sh
+++ Ubuntu24Desktop.sh
@@ -27,5 +27,9 @@ sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli container
 
 sudo apt-get install -y nextcloud-desktop
 
-
 COMMENTOUT
+
+sudo apt-get install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt-get update && sudo apt-get install caddy
